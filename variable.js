84a7c6e9becb45e15439ef07bb4ef0fdd087712a
starlabelgh <hello@starlabel.co>
2022-04-21 06:18:10 +0000

    Initial commit

---
+++ variable.js
@@ -0,0 +1,38 @@
+//let myName = 'Kofi Moses'
+//console.log(myName)
+
+let boolean = '2.02'
+console.log(boolean)
+
+let undefined
+console.log(undefined)
+
+let empty = ''
+console.log(null)
+
+console.log(typeof 507)
+
+    //comments can make code readable
+    //welcome to 30DaysofJavascript
+
+    /* 
+    comments can make code readable, easy to reuse and informative
+    */
+
+
+let variableOne
+let variableTwo
+let variableThree
+let variableFour
+
+let variable1 = '1'
+let variable2 ='two'
+let variable3 ='3'
+let variable4 = '4.5'
+
+let firstName = 'Moses'
+let lastName = 'Samanhyia'
+let age = 27
+let occupation = 'software developer'
+
+console.log(firstName, lastName, age, occupation)
